5c053a6fabc956e21f0f3bac017624bdbdc9dc85
lswmxl <128008687+lswmxl@users.noreply.github.com>
2024-11-26 15:24:38 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -62,21 +62,21 @@ install_singbox(){
 
 
     if [[ $SYSTEM == "CentOS" ]]; then
-        wget -N --no-check-certificate https://github.com/SagerNet/sing-box/releases/download/$version_tag/sing-box_"$download_tag"_linux_$(archAffix).rpm
+        wget -N --no-check-certificate https://5th.pp.ua/https://github.com/SagerNet/sing-box/releases/download/$version_tag/sing-box_"$download_tag"_linux_$(archAffix).rpm
         rpm -i sing-box_"$download_tag"_linux_$(archAffix).rpm
         rm -f sing-box_"$download_tag"_linux_$(archAffix).rpm
     else
-        wget -N --no-check-certificate https://github.com/SagerNet/sing-box/releases/download/$version_tag/sing-box_"$download_tag"_linux_$(archAffix).deb
+        wget -N --no-check-certificate https://5th.pp.ua/https://github.com/SagerNet/sing-box/releases/download/$version_tag/sing-box_"$download_tag"_linux_$(archAffix).deb
         dpkg -i sing-box_"$download_tag"_linux_$(archAffix).deb
         rm -f sing-box_"$download_tag"_linux_$(archAffix).deb
     fi
 
     rm -f /etc/sing-box/config.json
-    wget --no-check-certificate -O /etc/sing-box/config.json https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/server-config.json
+    wget --no-check-certificate -O /etc/sing-box/config.json https://5th.pp.ua/https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/server-config.json
     
     mkdir /root/sing-box
-    wget --no-check-certificate -O /root/sing-box/client-sockshttp.json https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/client-sockshttp.json
-    wget --no-check-certificate -O /root/sing-box/client-tun.json https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/client-tun.json
+    wget --no-check-certificate -O /root/sing-box/client-sockshttp.json https://5th.pp.ua/https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/client-sockshttp.json
+    wget --no-check-certificate -O /root/sing-box/client-tun.json https://5th.pp.ua/https://raw.githubusercontent.com/lanhebe/singbox-shadowtls/main/configs/client-tun.json
     
     wgcfv6status=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
     wgcfv4status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
